c6fd207367fa5bf66cfee6da6417fb181dbabf85
Shaodong Ma <1289490664@qq.com>
2020-07-27 21:59:20 +0800

    changed generateOrderArray() to generateOrderedArray()

--- 02-LinearSearch/cpp/09-Test-Algorithm-Performance/main.cc
+++ 02-LinearSearch/cpp/09-Test-Algorithm-Performance/main.cc
@@ -1,25 +1,16 @@
 #include <ctime>
 #include <iostream>
 #include <string>
+#include "ArrayGenerator.h"
 #include "LinearSearch.h"
 
 using namespace std;
-
-class ArrayGenerator {
- public:
-  ArrayGenerator() = delete;
-  static int *generateOrderArray(int n) {
-    int *data_ = new int[n];
-    for (int i = 0; i < n; ++i) data_[i] = i;
-    return data_;
-  }
-};
 
 int main() {
   int dataSize[] = {1000000, 10000000};
 
   for (auto n : dataSize) {
-    int *data = ArrayGenerator::generateOrderArray(n);
+    int *data = ArrayGenerator::generateOrderedArray(n);
     clock_t startTime = clock();
     for (int k = 0; k < 100; ++k) LinearSearch::search(data, n, n);
     clock_t endTime = clock();
